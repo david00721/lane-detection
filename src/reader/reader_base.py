e7a7ac2ee2cf2ec2317c404e061f9071f3f42631
david00721 <david007214@gmail.com>
2024-02-19 22:54:06 +0100

    fixed path collecting

--- src/reader/reader_base.py
+++ src/reader/reader_base.py
@@ -19,14 +19,21 @@ class CA2D2Reader:
         l_sceneFolders: list[str] = [d for d in os.listdir(self.m_datasetRoot) if os.path.isdir(os.path.join(self.m_datasetRoot, d))]
 
         for sceneFolder in l_sceneFolders:
-            subfolders = [f.path for f in os.scandir(os.path.join(self.m_datasetRoot, sceneFolder, "camera")) if f.is_dir()]
+            frames_folders = [f.path for f in os.scandir(os.path.join(self.m_datasetRoot, sceneFolder, "camera")) if f.is_dir()]
+            labels_folders = [f.path for f in os.scandir(os.path.join(self.m_datasetRoot, sceneFolder, "label")) if f.is_dir()]
 
-            for path in subfolders:
-                l_cameraImages = sorted([img for img in os.listdir(path) if os.path.splitext(img)[1].lower() in self.m_imageExtensions])
-                l_labelImages = sorted([img for img in os.listdir(path) if os.path.splitext(img)[1].lower() in self.m_imageExtensions])
+            self.collectFramePaths(frames_folders)
+            self.collectLabelPaths(labels_folders)
 
-                self.m_framePaths.extend([os.path.join(path, camera_img) for camera_img in l_cameraImages])
-                self.m_labelPaths.extend([os.path.join(path, label_img) for label_img in l_labelImages])
+    def collectFramePaths(self, f_folders: list[str]) -> None:
+        for path in f_folders:
+            l_cameraImages = sorted([img for img in os.listdir(path) if os.path.splitext(img)[1].lower() in self.m_imageExtensions])
+            self.m_framePaths.extend([os.path.join(path, camera_img) for camera_img in l_cameraImages])
+
+    def collectLabelPaths(self, f_folders: list[str]) -> None:
+        for path in f_folders:
+            l_labelImages = sorted([img for img in os.listdir(path) if os.path.splitext(img)[1].lower() in self.m_imageExtensions])
+            self.m_labelPaths.extend([os.path.join(path, label_img) for label_img in l_labelImages])
 
     def loadClasses(self) -> None:
         with open(self.m_classesPath, "r") as json_file:
